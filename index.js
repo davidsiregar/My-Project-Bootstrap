a9b91c939783496e31f7227696c4cd42722a4b7b
david siregar <davidsiregar699@gmail.com>
2022-03-13 18:43:25 +0700

    version:1.0.0

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+const app = express();
+const PORT = 4000;
+
+const isLogin = true;
+
+app.set("view engine", "hbs");
+
+app.use("/public", express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.get("/my-project", function (req, res) {
+  res.render("my-project");
+});
+app.get("/my-project-add", function (req, res) {
+  res.render("my-project-add");
+});
+app.post("/my-project-add", function (req, res) {
+  console.log(req.body);
+  console.log(req.body.name);
+  console.log(req.body.start);
+  console.log(req.body.end);
+  console.log(req.body.desc);
+  console.log(req.body.option);
+  console.log(req);
+  res.redirect("/");
+});
+app.get("/my-project-detail", function (req, res) {
+  res.render("my-project-detail");
+});
+app.get("/contact-me", function (req, res) {
+  res.render("contact-me");
+});
+
+app.listen(PORT, function () {
+  console.log(`Server starting on PORT: ${PORT}`);
+});
